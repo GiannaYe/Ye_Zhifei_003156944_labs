61edecaa7ce523ad4dc4023a0812d166fc4e6211
Whenindoubt-switchtocs <zy94@cornell.edu>
2025-09-14 20:06:43 -0700

    UI

--- Lab 1/Lab1/src/Model/Address.java
+++ Lab 1/Lab1/src/Model/Address.java
@@ -9,5 +9,42 @@ package Model;
  * @author yzf01
  */
 public class Address {
+    String streeName;
+    String unitNum;
+    String city;
+    String zipCode;
+
+    
+    public String getStreeName() {
+        return streeName;
+    }
+
+    public String getUnitNum() {
+        return unitNum;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setStreeName(String streeName) {
+        this.streeName = streeName;
+    }
+
+    public void setUnitNum(String unitNum) {
+        this.unitNum = unitNum;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
     
 }
--- Lab 1/Lab1/src/Model/Product.java
+++ Lab 1/Lab1/src/Model/Product.java
@@ -9,5 +9,61 @@ package Model;
  * @author yzf01
  */
 public class Product {
+    String name;
+    String description;
+    String availNum;
+    String price;
+    
+    Address manufactureAddress;
+    Address manufactureShippingAddress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Address getManufactureAddress() {
+        return manufactureAddress;
+    }
+
+    public void setManufactureAddress(Address manufactureAddress) {
+        this.manufactureAddress = manufactureAddress;
+    }
+
+    public Address getManufactureShippingAddress() {
+        return manufactureShippingAddress;
+    }
+
+    public void setManufactureShippingAddress(Address manufactureShippingAddress) {
+        this.manufactureShippingAddress = manufactureShippingAddress;
+    }
+
     
 }
