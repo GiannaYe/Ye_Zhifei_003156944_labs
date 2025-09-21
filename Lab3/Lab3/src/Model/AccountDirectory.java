0415d426c0b26e794acb01fa07d24b24885dc2df
Fibi <zy94@cornell.edu>
2025-09-21 16:09:23 -0700

    init Lab3

---
+++ Lab3/Lab3/src/Model/AccountDirectory.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Olive
+ */
+public class AccountDirectory {
+    private ArrayList<Account> accounts;
+
+    public AccountDirectory() {
+        this.accounts = new ArrayList<>();
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(ArrayList<Account> accounts) {
+        this.accounts = accounts;
+    }
+    
+    public Account addAccount() {
+        Account a = new Account();
+        accounts.add(a);
+        return a;
+    }
+    
+    public void deleteAccount(Account account) {
+        accounts.remove(account);
+    
+    }
+}
